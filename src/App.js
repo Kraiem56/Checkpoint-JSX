24c81fe1c9b468968caf3625e49e04312914da77
Kraiem56 <bkmohamed016@gmail.com>
2022-03-02 00:33:09 +0100

    gsx

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import imageInSrc from "./imageInSrc.PNG"
+import imageInPublic from "./imageInPublic.PNG";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='Named'>
+        <h1>Mohamed Ben Kraiem</h1>
+      
+      <br />
+      
+        <img className='imageInSrc' src={imageInSrc} alt="" />
+    <br />
+     <img className='imageInPublic' src={imageInPublic} alt="" />
+    </div>
+    <iframe
+    width="320" 
+    height="240" 
+    src="https://www.youtube.com/embed/Oo52vQyAR6w" 
+    title="YouTube video player" 
+    frameborder="0" 
+    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+    allowfullscreen></iframe>
     </div>
   );
 }
